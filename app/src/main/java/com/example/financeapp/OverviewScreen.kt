6d957feec3ca367340bc54f5d11d8ab72f3bbb8f
Lucas William <lucaswilliameufrasio@gmail.com>
2023-07-02 17:14:44 -0300

    feat: add a button to scroll to the top of the list of transactions

--- app/src/main/java/com/example/financeapp/OverviewScreen.kt
+++ app/src/main/java/com/example/financeapp/OverviewScreen.kt
@@ -1,13 +1,20 @@
 package com.example.financeapp
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowUpward
 import androidx.compose.material.icons.filled.ClearAll
+import androidx.compose.material.icons.filled.MoveUp
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -19,16 +26,28 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.financeapp.data.randomTransaction
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Overview(transactionViewModel: TransactionsViewModel = viewModel()) {
     val uiState by transactionViewModel.uiState.collectAsState()
+    val listState = rememberLazyListState()
+    val scope = rememberCoroutineScope()
+    val showScrollToTopButton by remember {
+        derivedStateOf {
+            listState.firstVisibleItemIndex > 7
+        }
+    }
 
     Scaffold(
         topBar = {
@@ -76,7 +95,8 @@ fun Overview(transactionViewModel: TransactionsViewModel = viewModel()) {
                 contentPadding = PaddingValues(
                     top = 16.dp,
                     bottom = 16.dp
-                )
+                ),
+                state = listState
             ) {
                 items(uiState.transactions.size) { index ->
                     TransactionCard(
@@ -86,6 +106,31 @@ fun Overview(transactionViewModel: TransactionsViewModel = viewModel()) {
                         date = uiState.transactions[index].date.formatDate()
                     )
                     Spacer(modifier = Modifier.padding(8.dp))
+                }
+            }
+
+        }
+
+        if (showScrollToTopButton) {
+            Box(modifier = Modifier.fillMaxSize()) {
+                IconButton(
+                    modifier = Modifier
+                        .align(Alignment.BottomCenter)
+                        .padding(16.dp)
+                        .background(
+                            MaterialTheme.colorScheme.primary,
+                            MaterialTheme.shapes.extraLarge
+                        ),
+                    onClick = {
+                        scope.launch {
+                            listState.animateScrollToItem(0)
+                        }
+                    }) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowUpward,
+                        tint = MaterialTheme.colorScheme.onPrimary,
+                        contentDescription = ""
+                    )
                 }
             }
         }
